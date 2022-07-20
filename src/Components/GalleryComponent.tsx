4e2035806c34a6d2b03ef0abec5be44f90fc390f
Mati-X <mateuszmatla25@gmail.com>
2022-07-20 17:34:00 +0100

    ewe

--- src/Components/GalleryComponent.tsx
+++ src/Components/GalleryComponent.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { wrap } from "popmotion";
-import { images } from "../data/image-data"
+import { images } from "./image-data"
 import { Icon } from '@iconify/react';
 
 const variants = {
